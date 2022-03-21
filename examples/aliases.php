266a484089618bbbcfffb379b07c5c929e187f26
Алексей Юдов <tcloud.ax@gmail.com>
2022-03-21 06:26:43 +0300

    example update

--- examples/aliases.php
+++ examples/aliases.php
@@ -5,26 +5,24 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use isDayOff\Client\IsDayOff;
 
 $client = new IsDayOff();
-$date = new DateTime();
-$tomorrow = clone $date;
-$tomorrow->modify('+1 day');
+
+// Tomorrow example 
+$tomorrowDate = new DateTime();
+$tomorrowDate->modify('+1 day');
 $result = $client->date()->tomorrow();
 
-if($result)
-{
-    echo $tomorrow->format('Y-m-d') . ' is day off.' . PHP_EOL;
-}
-else
-{
-    echo $tomorrow->format('Y-m-d') . ' working day.' . PHP_EOL;
+if($result) {
+    echo $tomorrowDate->format('Y-m-d') . ' is day off.' . PHP_EOL;
+} else {
+    echo $tomorrowDate->format('Y-m-d') . ' working day.' . PHP_EOL;
 }
 
+// Today example 
+$todayDate = new DateTime();
 $result = $client->date()->today();
-if($result)
-{
-    echo $date->format('Y-m-d') . ' is day off.' . PHP_EOL;
-}
-else
-{
-    echo $date->format('Y-m-d') . ' working day.' . PHP_EOL;
+
+if($result) {
+    echo $todayDate->format('Y-m-d') . ' is day off.' . PHP_EOL;
+} else {
+    echo $todayDate->format('Y-m-d') . ' working day.' . PHP_EOL;
 }
